925509c4c77e81ce9d127b9e34b30784ffbfb99c
jaber <jaber@acornassoicated.org>
2026-04-23 14:18:54 +0300

    udpate

--- plugins/store/store/Plugin.php
+++ plugins/store/store/Plugin.php
@@ -22,43 +22,43 @@ class Plugin extends PluginBase
     {
     }
 
- public function registerNotificationRules()
-    {
-        return [
-            'events' => [
-                \Store\Store\NotifyRules\SizeEvent::class,
-            ],
-            'actions' => [
-                \Winter\Notify\NotifyRules\SaveDatabaseAction::class, 
-            ],
-            'groups' => [
-                'store' => [
-                    'label' => 'المتجر',
-                    'icon' => 'icon-shopping-cart'
-                ],
-            ],
-            'presets' => '$/store/store/config/notify_presets.yaml',
-        ];
-    }
+//  public function registerNotificationRules()
+//     {
+//         return [
+//             'events' => [
+//                 \Store\Store\NotifyRules\SizeEvent::class,
+//             ],
+//             'actions' => [
+//                 \Winter\Notify\NotifyRules\SaveDatabaseAction::class, 
+//             ],
+//             'groups' => [
+//                 'store' => [
+//                     'label' => 'المتجر',
+//                     'icon' => 'icon-shopping-cart'
+//                 ],
+//             ],
+//             'presets' => '$/store/store/config/notify_presets.yaml',
+//         ];
+//     }
 
-    public function boot()
-    {
-        // ربط جميع أحداث المقاسات بنفس الحدث
-        \Winter\Notify\Classes\Notifier::bindEvents([
-            'store.size.created' => \Store\Store\NotifyRules\SizeEvent::class,
-            'store.size.updated' => \Store\Store\NotifyRules\SizeEvent::class,
-            'store.size.deleted' => \Store\Store\NotifyRules\SizeEvent::class,
-        ]);
+//     public function boot()
+//     {
+//         // ربط جميع أحداث المقاسات بنفس الحدث
+//         \Winter\Notify\Classes\Notifier::bindEvents([
+//             'store.size.created' => \Store\Store\NotifyRules\SizeEvent::class,
+//             'store.size.updated' => \Store\Store\NotifyRules\SizeEvent::class,
+//             'store.size.deleted' => \Store\Store\NotifyRules\SizeEvent::class,
+//         ]);
         
-        // تسجيل قناة قاعدة البيانات
-        \Winter\Notify\NotifyRules\SaveDatabaseAction::extend(function ($action) {
-            $action->addTableDefinition([
-                'label'    => 'إشعارات المقاسات',
-                'class'    => \Store\Store\Models\Size::class,
-                'relation' => 'notifications',
-                'param'    => 'size'
-            ]);
-        });
-    }
+//         // تسجيل قناة قاعدة البيانات
+//         \Winter\Notify\NotifyRules\SaveDatabaseAction::extend(function ($action) {
+//             $action->addTableDefinition([
+//                 'label'    => 'إشعارات المقاسات',
+//                 'class'    => \Store\Store\Models\Size::class,
+//                 'relation' => 'notifications',
+//                 'param'    => 'size'
+//             ]);
+//         });
+//     }
 
 }
